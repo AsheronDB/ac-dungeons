198ac039349441813285ffe845020acea6f3319d
Jesse L.K. Overton <jesse@ovrtn.com>
2024-04-09 21:15:10 -0400

    Add Knorr Lyceum dungeons

--- build.ts
+++ build.ts
@@ -1,153 +1,52 @@
 import { groupBy, forEach as _forEach } from "lodash-es";
-import { lstatSync, readdirSync, readFileSync, writeFileSync } from "fs";
+import { unlinkSync, existsSync, lstatSync, readdirSync, readFileSync, writeFileSync } from "fs";
 import { join } from "path";
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite'
 
-const allPaths = [];
+const dbPath = './db/ac_locations.db'
+
+if (existsSync(dbPath)) unlinkSync(dbPath);
 
 
+function getAllLocationFiles() {
 
-function traverseDir(dir) {
-  readdirSync(dir).forEach((file) => {
-    let fullPath = join(dir, file);
-    if (lstatSync(fullPath).isDirectory()) {
-      traverseDir(fullPath);
-    } else {
-      // console.log(file);
-      if (!/(^|\/)\.[^\/\.]/g.test(file)) {
-        // console.log('valid file')
-        allPaths.push(fullPath);
-      }
-    }
-  });
-}
+  const allPaths = [];
 
-traverseDir("./src/regions");
-
-// const zonesFolder = "./src/regions/";
-// let zoneFiles = readdirSync(zonesFolder);
-
-// allFiles = allFiles.filter((item) => );
-
-const zones = [];
-
-allPaths.forEach((filePath) => {
-  // console.log(file);
-  const data = readFileSync(filePath, "utf8");
-  const json = JSON.parse(data);
-  zones.push(json);
-});
-
-const dupeNames = Object.entries(groupBy(zones, 'name')).filter(group => group[1].length > 1);
-
-console.log(dupeNames);
-
-// console.log(zones);
-
-const landblocks = {};
-
-function buildTree(nodes) {
-  // const treeArray = [];
-
-  // const idMap = nodes.reduce((obj, node) => {
-
-  //   if (obj[node.id]) {
-  //     console.log('item ID already found on object');
-  //       console.log(node)
-  //   } else {
-  //     Object.assign(obj, {
-  //       [node.id]: node,
-  //     });
-  //   }
-
-  //   return obj;
-  // }, {});
-
-  // console.log(idMap);
-
-  // const recurseChildren = (node) => {
-  //   const childNodes = [];
-
-  //   console.log('Current node')
-  //   console.log(node);
-  //   node.children.forEach((childId: string) => {
-  //     const childNode = idMap[childId];
-  //     childNodes.push(childNode);
-  //     delete idMap[childId];
-  //     console.log('recursing children')
-  //     console.log('childId', childId)
-
-  //     if (childNode.children && childNode.children.length) {
-  //       childNode.children = recurseChildren(childNode);
-  //     }
-  //   });
-  //   return childNodes;
-  // };
-
-  // nodes.forEach((node) => {
-
-  //   if (!idMap[node.id]) return false;
-
-  //   if (node.children && node.children.length) {
-
-  //     node.children = recurseChildren(node);
-  //   }
-  //   treeArray.push(node);
-  // });
-  // return treeArray;
-
-  const map = {};
-
-  function findChildren(children) {
-
-    // console.log('Find children')
-    // console.log(children)
-    let childNodes = children.map((childId) => {
-      // console.log('findChildren children.map');
-      // console.log(childId);
-      const mapId = map[childId];
-      // console.log(mapId);
-      return mapId;
-    });
-
-    // console.log(childNodes);
-
-    childNodes.forEach((node) => {
-
-      if (!map[node.id]) return false;
-      delete map[node.id]
-      if (node.children && node.children.length > 0) {
-        node.children = findChildren(node.children);
-
-        //node.children = node.children.map(childId => map[childId]);
+  function traverseDir(dir) {
+    readdirSync(dir).forEach((file) => {
+      let fullPath = join(dir, file);
+      if (lstatSync(fullPath).isDirectory()) {
+        traverseDir(fullPath);
+      } else {
+        // console.log(file);
+        if (!/(^|\/)\.[^\/\.]/g.test(file)) {
+          // console.log('valid file')
+          allPaths.push(fullPath);
+        }
       }
     });
-
-    return childNodes;
   }
 
+  traverseDir("./src/regions");
 
+  const zones = [];
+  const locations = {};
 
-  // Create a map of IDs to objects
-  nodes.forEach((node) => {
-    map[node.id] = node;
+  allPaths.forEach((filePath) => {
+    // console.log(file);
+    const data = readFileSync(filePath, "utf8");
+    const json = JSON.parse(data);
+    locations.push(json);
   });
 
-  // Build the nested structure
-  nodes.forEach((node) => {
-
-    if (!map[node.id]) return false;
-    if (node.children && node.children.length > 0) {
-      node.children = findChildren(node.children);
-
-      //node.children = node.children.map(childId => map[childId]);
-    }
-  });
-
-  // Find root objects
-  // const rootObjects = nodes.filter(node => !node.children);
-
-  return nodes;
+  return locations;
 }
+
+const locations = getAllLocationFiles();
+
+const dupeNames = Object.entries(groupBy(locations, 'name')).filter(group => group[1].length > 1);
+console.log(dupeNames);
 
 function createTree(objects) {
   // Create a map of objects by their ID for easy lookup
@@ -178,13 +77,9 @@ function traverseTreeMultipleRoots(roots: [], path = [], paths = []) {
   // Iterate over each root node
   for (const root of roots) {
     // Add the current root node's name to the path
-    path.push(root.name);
 
-    if (root.name == "Arrival Chamber") {
-      console.log("ARRIVAL FACILITY!");
-      console.log(root);
-      console.log(path);
-    }
+    path.push({ id: root.id, rank: path.length });
+
 
     if (root.cells) {
       root.cells.forEach((cell: string) => {
@@ -229,11 +124,90 @@ function traverseTreeMultipleRoots(roots: [], path = [], paths = []) {
   return paths;
 }
 
-const tree = createTree(zones);
+const tree = createTree(locations);
 
 // console.log(tree);
 
 traverseTreeMultipleRoots(tree);
+
+const createLocationsTableQuery = `CREATE TABLE locations (id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  label TEXT NULL,
+	uuid TEXT NOT NULL,
+	type TEXT NOT NULL,
+  category TEXT NULL
+);`
+
+const createCellsTableQuery = `CREATE TABLE cells (id INTEGER PRIMARY KEY,
+  obj_cell_id INTEGER NOT NULL,
+  location_id INTEGER NOT NULL,
+  rank NOT NULL,
+  FOREIGN KEY (location_id) REFERENCES locations(id)
+);`
+
+async function createSqliteDB() {
+
+  const db = await open({
+    filename: dbPath,
+    driver: sqlite3.Database
+  })
+
+  await db.exec(createLocationsTableQuery);
+  await db.exec(createCellsTableQuery);
+
+
+  const landblockEntries = Object.entries(landblocks);
+
+  for (let i = 0; i < landblockEntries.length; i++) {
+    const [landblockId, cells] = landblockEntries[i];
+    const cellEntries = Object.entries(cells);
+    for (let j = 0; j < cellEntries.length; j++) {
+      const [cellId, locationsArray] = cellEntries[j];
+      const objCellId = parseInt(`${landblockId}${cellId}`, 16);
+
+      for (let k = 0; k < locationsArray.length; k++) {
+
+        const location = locationsArray[k];
+        const locationObj = locations.find(zone => zone.id === location.id);
+
+
+        if (!locationObj) return false;
+
+
+
+        const findLocationQuery = `SELECT id, uuid FROM locations WHERE uuid = ?`;
+        const insertLocationQuery = `INSERT INTO locations (name, uuid, type, category, label) VALUES(?, ?, ?, ?, ?) RETURNING id`;
+        const insertCellQuery = `INSERT INTO cells (obj_cell_id, location_id, rank) VALUES(?, ?, ?)`;
+
+        const locRow = await db.get(findLocationQuery, location.id);
+
+        let insertedId;
+
+        if (!locRow) {
+          console.log("No location row found, inserting...")
+          const insertValues = [locationObj.name, locationObj.id, 'region', null, locationObj.label || null];
+          const insertedRow = await db.get(insertLocationQuery, insertValues);
+
+          console.log(insertedRow);
+          insertedId = insertedRow.id;
+        } else {
+          insertedId = locRow.id;
+        }
+        console.log("Adding cell: ", objCellId)
+        await db.run(insertCellQuery, [objCellId, insertedId, location.rank]);
+
+      }
+    }
+
+  }
+
+  await db.close()
+
+
+}
+
+createSqliteDB();
+
 
 writeFileSync("./export/all-dungeons.json", JSON.stringify(tree, null, 2));
 writeFileSync(
